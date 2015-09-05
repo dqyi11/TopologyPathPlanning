b7ea255992fe9b7748979565b5dc7511e565adf9
walter <dqyi11@gmail.com>
2015-09-04 20:53:25 -0600

    add new

--- src/homotopy/reference_frames.cc
+++ src/homotopy/reference_frames.cc
@@ -1,14 +1,40 @@
 #include "reference_frames.h"
 
-ReferenceFrames::ReferenceFrames( WorldMap* p_world_map ) {
-    _p_world_map = p_world_map;
+ReferenceFrame::ReferenceFrame() {
 }
 
-ReferenceFrames::~ReferenceFrames() {
+ReferenceFrame::~ReferenceFrame() {
 
 }
 
-StringGrammar* ReferenceFrames::get_string_grammar( SubRegion* p_init, SubRegion* p_goal ) {
+ReferenceFrameSet::ReferenceFrameSet( WorldMap* p_world_map ) {
+  _p_world_map = p_world_map;
+  _reference_frames.clear();
+  for( std::vector<LineSubSegmentSet*>::iterator it = _p_world_map->get_sublinesegment_set().begin();
+       it != _p_world_map->get_sublinesegment_set().end(); it ++ ) {
+    LineSubSegmentSet* p_linesubsegment_set = (*it);
+    for( std::vector<LineSubSegment*>::iterator its = p_linesubsegment_set->m_subsegs.begin();
+         its != p_linesubsegment_set->m_subsegs.end(); its ++ ) {
+      LineSubSegment* p_linesubsegment = (*its);
+      ReferenceFrame* p_rf = new ReferenceFrame();
+      p_rf->m_name = p_linesubsegment->get_name();
+      p_rf->m_segment = p_linesubsegment->m_subseg;
+      _reference_frames.push_back(p_rf);
+    }
+  }  
+}
+
+ReferenceFrameSet::~ReferenceFrameSet() {
+  for( std::vector<ReferenceFrame*>::iterator it = _reference_frames.begin();
+       it != _reference_frames.end(); it ++ ) {
+    ReferenceFrame* p_rf = (*it);
+    delete p_rf;
+    p_rf = NULL;
+  }
+  _reference_frames.clear();
+}
+
+StringGrammar* ReferenceFrameSet::get_string_grammar( SubRegion* p_init, SubRegion* p_goal ) {
   StringGrammar* p_grammar = NULL;
   if( _p_world_map || p_init || p_goal ) {
     p_grammar = new StringGrammar();  
@@ -29,7 +55,7 @@ StringGrammar* ReferenceFrames::get_string_grammar( SubRegion* p_init, SubRegion
   return p_grammar;
 }
 
-HomotopicGrammar* ReferenceFrames::get_homotopic_grammar( SubRegion* p_init, SubRegion* p_goal ) {
+HomotopicGrammar* ReferenceFrameSet::get_homotopic_grammar( SubRegion* p_init, SubRegion* p_goal ) {
   HomotopicGrammar* p_grammar = NULL;
   if( _p_world_map ) {
     p_grammar = new HomotopicGrammar();
@@ -38,26 +64,22 @@ HomotopicGrammar* ReferenceFrames::get_homotopic_grammar( SubRegion* p_init, Sub
   return p_grammar;
 }
 
-std::string ReferenceFrames::get_character_id( Point2D start, Point2D end, grammar_type_t type ) {
-   std::string character_id = "";
-   Segment2D line(start, end);
-   if (type == STRING_GRAMMAR_TYPE) {
-     for( std::vector<LineSubSegmentSet*>::iterator it = _p_world_map->get_sublinesegment_set().begin();
-          it != _p_world_map->get_sublinesegment_set().end(); it ++ ) {
-       LineSubSegmentSet* p_linesubsegment_set = (*it);
-       for( std::vector<LineSubSegment*>::iterator itl = p_linesubsegment_set->m_subsegs.begin();
-            itl != p_linesubsegment_set->m_subsegs.end(); itl ++ ) {
-          LineSubSegment* p_linesubsegment = (*itl);
-          CGAL::Object result = intersection( p_linesubsegment->m_subseg, line );
-          Point2D po;
-          if ( CGAL::assign( po, result ) ) {
-            return p_linesubsegment->get_name();
-          }
-       }
-     }
-   }
-   else if (type == HOMOTOPIC_GRAMMAR_TYPE) {
+std::string ReferenceFrameSet::get_character_id( Point2D start, Point2D end, grammar_type_t type ) {
+  std::string character_id = "";
+  Segment2D line(start, end);
+  if (type == STRING_GRAMMAR_TYPE) {
+    for( std::vector<ReferenceFrame*>::iterator it = _reference_frames.begin();
+         it != _reference_frames.end(); it ++ ) {
+      ReferenceFrame* p_rf = (*it);
+      CGAL::Object result = intersection( p_rf->m_segment, line );
+      Point2D po;
+      if ( CGAL::assign( po, result ) ) {
+        return p_rf->m_name;
+      }
+    }
+  }
+  else if (type == HOMOTOPIC_GRAMMAR_TYPE) {
 
-   }        
-   return character_id;
+  }        
+  return character_id;
 }
--- src/homotopy/reference_frames.h
+++ src/homotopy/reference_frames.h
@@ -10,11 +10,20 @@ typedef enum {
     HOMOTOPIC_GRAMMAR_TYPE
 } grammar_type_t;
 
-class ReferenceFrames {
+class ReferenceFrame {
+public:
+  ReferenceFrame();
+  virtual ~ReferenceFrame();
+  
+  std::string m_name; 
+  Segment2D   m_segment;
+};
+
+class ReferenceFrameSet {
 
 public:
-    ReferenceFrames( WorldMap* p_world_map );
-    virtual ~ReferenceFrames();
+    ReferenceFrameSet( WorldMap* p_world_map );
+    virtual ~ReferenceFrameSet();
 
     StringGrammar* get_string_grammar( SubRegion* p_init, SubRegion* p_goal );
     HomotopicGrammar* get_homotopic_grammar( SubRegion* p_init, SubRegion* p_goal );
@@ -22,7 +31,8 @@ public:
     std::string get_character_id( Point2D start, Point2D end, grammar_type_t type );
 
 private:
-    WorldMap*         _p_world_map;
+    WorldMap*                    _p_world_map;
+    std::vector<ReferenceFrame*> _reference_frames;
 };
 
 #endif // REFERENCE_FRAMES_H
